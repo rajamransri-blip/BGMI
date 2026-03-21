b49ac416db6ce46947b6ce0d763d120886870c79
rajamransri-blip <rajamransri@gmail.com>
2026-03-21 22:11:48 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/bgmiuc/MainActivity.kt
+++ android/app/src/main/kotlin/com/bgmiuc/MainActivity.kt
@@ -3,6 +3,4 @@ package com.bgmiuc
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    // V2 embedding ke sath ye class by default empty reh sakti hai.
-    // FlutterActivity automatically sab handle kar lega.
 }
